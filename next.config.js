7a95f9f9bc530ad01e21de1927d01a9d4ae77f57
BGMCandy <nielsbonde992@gmail.com>
2025-09-26 14:13:08 +0200

    Remove static export - fix Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Remove static export - let Vercel handle it
+}
+
+module.exports = nextConfig
---
+++ src/app/simple/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>ESCALATOR TRUMP</h1>
+      <p>Game is working!</p>
+    </div>
+  );
+}
